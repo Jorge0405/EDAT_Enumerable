39899e13c94555a64c168642b8a4b5f87f32ca98
Jorge0405 <23030515@itesa.edu.mx>
2024-11-20 08:19:48 -0600

    Version_02: Lista con foreach, for, clear, sort, orderby

--- Enumerable.UI.Consola/Program.cs
+++ Enumerable.UI.Consola/Program.cs
@@ -2,6 +2,40 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("===========================================");
+        Console.WriteLine("Enumerables");
+        Console.WriteLine("===========================================");
+
+        #region Lista
+        var lista = new List<string>
+        {
+            "Juan",
+            "Carlos",
+            "Jose",
+            "Dania",
+            "Pedro",
+            "Daniel",
+            "Josefa",
+            "Jorge",
+            "Luis",
+            "Kevin"
+        };
+
+        lista.Add("Sonic");
+        //foreach (var nombre in lista)
+        //{
+        //    Console.WriteLine(nombre);
+        //}
+        lista.OrderBy(lista);
+
+
+        for (int i = 0; i < lista.Count; i++)
+        {
+            Console.WriteLine(lista[i]);
+        }
+
+        #endregion
+
+        Console.ReadKey();
     }
 }
